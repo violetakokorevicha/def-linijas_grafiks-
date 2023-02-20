656964f8f6962fb1e54ae3d3473359aaabd88f70
violetakokorevi <91872388+violetakokorevicha@users.noreply.github.com>
2023-02-20 06:56:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+def linijas_grafiks():
+  x = np.arange(1, 21)
+  y = x
+ 
+
+  plt.title("Line graph")
+  plt.xlabel("X axis")
+  plt.ylabel("Y axis")
+  plt.plot(x, y, color ="purple")
+  plt.show()
+linijas_grafiks()
+
+
+
+
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+ 
